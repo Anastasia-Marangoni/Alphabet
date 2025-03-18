246163bb051d6a32252682d3d1da2fad4257323b
Anastasia-Marangoni <anastasia00m@gmail.com>
2025-03-18 13:11:05 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 export const configurazione = {
-  testo: "Anastasia Marangoni",
-  dimensione: 0.4,
-  interlinea: 0.4,
+  testo: "A",
+  dimensione: 0.9,
+  interlinea: 0.1,
 
   allineamento: "centro",
 
@@ -23,7 +23,7 @@ export const configurazione = {
  * @property {number} volume - Il volume del microfono
  * @property {number} [alpha] - Device orientation alpha angle (z-axis rotation)
  * @property {number} [beta] - Device orientation beta angle (front-to-back tilt)
- * @property {number} [gamma] - Device orientation gamma angle (left-to-right tilt)
+ * @property {number1} [gamma] - Device orientation gamma angle (left-to-right tilt)
  *
  * @param {Ingredienti} ingredienti
  */
@@ -38,32 +38,19 @@ export function disegnaPunto({
   beta = 0,
   gamma = 0,
 }) {
+  // rectMode(CENTER);
+  // push();
+  // translate(x, y);
+  // rotate(angolo);
+  // rect(0, 0, 0, 20);
+  // ellipse(0, 0, volume * 50, volume * 50);
+  // pop();
+
   push();
   translate(x, y);
-
-  noFill();
-  stroke(0);
-
-  // Use orientation data to influence color
-  // Map alpha (z-rotation) to hue (0-360)
-  const hue = map(alpha, 0, 360, 0, 360);
-
-  // Map beta (front-to-back tilt) to saturation (50-100)
-  const saturation = map(abs(beta), 0, 90, 50, 100);
-
-  // Map gamma (left-to-right tilt) to brightness (50-100)
-  const brightness = map(abs(gamma), 0, 90, 50, 100);
-
-  colorMode(HSB, 360, 100, 100);
-  fill(hue, saturation, brightness);
-  noStroke();
-
-  rectMode(CENTER);
-  rotate(frameCount + indice);
-
-  // Add slight variation based on device tilt
-  scale(1 + volume * 10 + (abs(gamma) / 90) * 0.5);
-  rect(0, 0, unita / 2);
+  rotate(angolo + 90);
+  let lung = mouseX;
+  line(-lung, 0, lung, 0);
   pop();
 }
 
@@ -83,8 +70,8 @@ export function impostazioni() {
 export function sotto(disegnaTesto) {
   background(255);
 
-  fill("deeppink");
-  disegnaTesto();
+  // fill("deeppink");
+  // disegnaTesto();
 }
 
 /**
@@ -92,7 +79,7 @@ export function sotto(disegnaTesto) {
  * @param {function} disegnaTesto - La funzione che disegna il testo
  */
 export function sopra(disegnaTesto) {
-  //   stroke("white");
+  // stroke("white");
   //   noFill();
   //   disegnaTesto();
 }
